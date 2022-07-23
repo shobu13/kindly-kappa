31df9db675804db673129f6a2b50178edafe2b1c
Kappa <72104527+kappq@users.noreply.github.com>
2022-07-23 21:51:50 +0200

    Rooms (#13)
    * Remove chat-app demo
    
    The server can still be tested with any WebSocket client (the websockets library has one).
    
    * Remove useless send method on ConnectionManager
    
    * Add support for rooms
    
    * feat: Add option for broadcasting to all or all except current user (#16)
    
    * feat: Adds an option to broadcast messages to everyone or everyone but the user who created the room
    
    * This can be useful for when sending data to another user upon joining the same room as the room owner doesn't need to receive what they already have on their editor
    
    * Add type hint for the everyone parameter
    
    Co-authored-by: Kappa <72104527+kappq@users.noreply.github.com>
    
    * Change route and endpoint name
    
    Co-authored-by: Luke <68163698+LukeHankey@users.noreply.github.com>

--- server/main.py
+++ server/main.py
@@ -2,10 +2,11 @@
 
 This server handles user connection, disconnection and events.
 """
-from uuid import uuid4
+from dataclasses import dataclass
+from typing import TypedDict
+from uuid import UUID, uuid4
 
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -48,6 +49,21 @@ class Client:
         return await self._websocket.receive_json()
 
 
+@dataclass
+class RoomData:
+    """A dataclass for data about a specific room."""
+
+    owner_id: UUID
+    clients: set[Client]
+
+
+class ActiveRooms(TypedDict):
+    """A data structure for active rooms."""
+
+    name: str
+    data: RoomData
+
+
 class ConnectionManager:
     """Manager for the WebSocket clients."""
 
@@ -56,115 +72,71 @@ class ConnectionManager:
 
         It stores the active connections and is able to broadcast data.
         """
-        self._active_connections: set[Client] = set()
+        self._rooms: ActiveRooms = {}
 
-    async def connect(self, client: Client) -> None:
-        """Accepts the connection and adds it to the active connections.
+    async def connect(self, client: Client, room: str) -> None:
+        """Accepts the connection and adds it to a room.
+
+        If the room doesn't exist, create it.
 
         Args:
             client: The Client to which the connection belongs.
+            room: The room to which the client will be connected.
         """
         await client.accept()
-        self._active_connections.add(client)
 
-    def disconnect(self, client: Client) -> None:
+        if room not in self._rooms:
+            self._rooms[room] = {"owner_id": client.id, "clients": set()}
+        self._rooms[room]["clients"].add(client)
+
+    def disconnect(self, client: Client, room: str) -> None:
         """Removes the connection from the active connections.
+
+        If, after the disconnection, the room is empty, delete it.
 
         Args:
             client: The Client to which the connection belongs.
+            room: The room from which the client will be disconnected.
         """
-        self._active_connections.remove(client)
+        self._rooms[room]["clients"].remove(client)
 
-    async def send(self, data: dict, client: Client) -> None:
-        """Sends data to a given client.
+        if len(self._rooms[room]["clients"]) == 0:
+            del self._rooms[room]
 
-        Args:
-            data: The data to be sent to the client, it should always contain a
-                "type" key to indicate the event type.
-            client: The client that will receive the data.
-        """
-        await client.send(data)
-
-    async def broadcast(self, data: dict) -> None:
+    async def broadcast(self, data: dict, room: str, everyone: bool = False) -> None:
         """Broadcasts data to all active connections.
 
         Args:
             data: The data to be sent to the clients, it should always contain a
                 "type" key to indicate the event type.
+            room: The room to which the data will be sent.
         """
-        for connection in self._active_connections:
-            await connection.send(data)
+        for connection in self._rooms[room]["clients"]:
+            if everyone:
+                await connection.send(data)
+            else:
+                if connection.id == self._rooms[room]["owner_id"]:
+                    continue
+                await connection.send(data)
 
 
 manager = ConnectionManager()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <input id="input" type="text" autocomplete="off">
-        <button onclick="sendMessage(event)">Send</button>
-        <ul id="messages"></ul>
-        <script>
-            let ws = new WebSocket("ws://localhost:8000/ws");
 
-            ws.onmessage = function(event) {
-                let data = JSON.parse(event.data);
-                switch (data.type) {
-                    case "message":
-                        let messages = document.getElementById("messages");
-                        let message = document.createElement("li");
-                        let content = document.createTextNode(data.msg);
-                        message.appendChild(content);
-                        messages.appendChild(message);
-                        break;
-                    default:
-                        console.log(`Unknown event type '${data.type}'`);
-                }
-            };
-
-            function sendMessage(event) {
-                let input = document.getElementById("input");
-                let data = {type: "message", msg: input.value};
-                ws.send(JSON.stringify(data));
-                input.value = "";
-                event.preventDefault();
-            }
-        </script>
-    </body>
-</html>
-"""
-
-
-@app.get("/")
-async def chat() -> HTMLResponse:
-    """This is the index route for the app.
-
-    Returns:
-        The HTML page in which the user can chat.
-    """
-    return HTMLResponse(html)
-
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket) -> None:
+@app.websocket("/room/{room_name}")
+async def room(websocket: WebSocket, room_name: str) -> None:
     """This is the endpoint for the WebSocket connection.
 
-    It creates a client and handles the connection with the ConnectionManager.
-    It continuosly receives, sends and broadcasts data to the active clients.
+    It creates a client and handles connection and disconnection with the
+    ConnectionManager. It continuously receives and broadcasts data to the
+    active clients.
     """
     client = Client(websocket)
-    await manager.connect(client)
-    await manager.broadcast({"type": "message", "msg": f"{client.id} joined the chat"})
+    await manager.connect(client, room_name)
 
     try:
         while True:
             data = await client.receive()
-            await manager.broadcast(data)
+            await manager.broadcast(data, room_name, everyone=True)
     except WebSocketDisconnect:
-        manager.disconnect(client)
-        await manager.broadcast({"type": "message", "msg": f"{client.id} left the chat"})
+        manager.disconnect(client, room_name)
